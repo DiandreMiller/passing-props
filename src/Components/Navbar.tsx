5be7a48413674712437efc2e63dd37b54bac27b6
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-05-06 16:53:55 -0400

    updated Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = ({count}) => {
     return (
         <div>
             <h1>This is the navbar</h1>
-            <h2>Navbar Count{count}</h2>
+            <h2>Navbar Count: {count}</h2>
         </div>
     )
 }
